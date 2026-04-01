92295da75462cb6cc4191ed1835f73d016d4e005
Kuber Budhathoki <koobear99@gmail.com>
2026-04-01 22:23:27 +1100

    comments and resources added

--- phase1/learn-terraform-get-started-aws/main.tf
+++ phase1/learn-terraform-get-started-aws/main.tf
@@ -12,22 +12,22 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 # Create a VPC
-resource "aws_vpc" "example" {
-  cidr_block = "10.0.0.0/16"
-}
-
-# resource "aws_instance" "app_server" {
-#   ami           = data.aws_ami.ubuntu.id
-#   instance_type = "t2.micro"
-#   monitoring    = true
-
-#   metadata_options {
-#     http_endpoint               = "enabled"
-#     http_tokens                 = "required"
-#     http_put_response_hop_limit = 1
-#   }
-
-#   tags = {
-#     Name = "learn-terraform"
-#   }
+# resource "aws_vpc" "example" {
+#   cidr_block = "10.0.0.0/16"
 # }
+# some kind of error... no idea need soe thing to do 
+resource "aws_instance" "app_server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  monitoring    = true
+
+  metadata_options {
+    http_endpoint               = "enabled"
+    http_tokens                 = "required"
+    http_put_response_hop_limit = 1
+  }
+
+  tags = {
+    Name = "learn-terraform"
+  }
+}
